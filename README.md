2e075da4085635e2316d25b1e8dfc96d6cb6e386
Ariel Guzman <ariel.guzman01@gmail.com>
2014-12-21 11:45:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+ngSrv Documentation
+====================
+
+A simple command-line tool for creating Angular.js modules based on restful resources
+
+Usage: ng-srv [-OPTIONS] <filename>
+-----------------------------------
+Options:
+--------
+    -h, --help             output usage information
+    -V, --version          output the version number
+    -a, --auth <filename>  create the resource file with a default auth factory
+
+Instructions:
+-------------
+You can use the command on the following ways
+
+    $ ng-srv nameOfFile         this will create the default file at the current directory
+    $ ng-srv -a nameOfFile      this will create a file with a declared Auth factory using tokens and $http interceptors
+
+Using the command with any of the options will prompt to:
+
+* Enter the module name. (Default to ngSrv)
+* Enter the default url.
+e.g.
+>> http://localhost/api/v1/ (make sure it ends with a slash)
+* Enter the path name.
+e.g.
+>> products
+* Enter the factory name for the current path
+e.g.
+>> product
+
+Once you are done, press the return key or enter 'exit' to finish.
+
+
+Authors:
+-------
+Ariel Guzman
+
+ - [https://github.com/arielgpe](https://github.com/arielgpe)
+ - [http://twitter.com/arielgpe](http://twitter.com/arielgpe)
+
+LICENSE:
+--------
+ngSrv is licensed under the MIT Open Source license. For more information, see the LICENSE file in this repository.
---
+++ main.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+
+//TODO comments
+
+var program = require('commander');
+var fs = require('fs');
+var readline = require('readline');
+var ejs = require('ejs');
+var chalk = require('chalk');
+
+var baseURL = '';
+var factories = [];
+var filename = '';
+var moduleName = 'ngSrv';
+
+var servicesTemplate = fs.readFileSync(
+    require.resolve('./templates/angular-service.template.ejs'),
+    { encoding: 'utf-8' }
+);
+
+var authTemplate = fs.readFileSync(
+    require.resolve('./templates/angular-auth.template.ejs'),
+    { encoding: 'utf-8' }
+);
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+program
+    .version('0.8.0')
+    .usage('[-OPTIONS] <filename>')
+    .option('-a, --auth <filename>', 'create the resource file with a default auth factory')
+    .parse(process.argv);
+
+
+if (program.auth){
+    filename = program.auth + '.js'
+} else {
+    filename = program.args + '.js'
+}
+console.log(chalk.cyan.bold('Filename: ' + filename));
+rl.question("Pick a module name (press enter for default: ngSrv): ", function(name){
+    if(name){
+        moduleName = name
+    }
+    rl.question("What is your API base URL? ", function(answer){
+        if(answer.length > 0){
+            baseURL = answer;
+            console.log("");
+            nameMethods()
+        } else {
+            console.log(chalk.red.bold('Emtpy url. Aborting!'));
+            process.exit(1);
+            rl.close();
+        }
+    });
+});
+
+var nameMethods = function() {
+    console.log("Write 'exit' or press the return key to finish");
+    rl.question("Enter the path name: ", function(path){
+        if (path === 'exit' || path.length === 0){
+            saveFile();
+            rl.close()
+        } else {
+            rl.question("Enter Factory name (singular): ", function(method){
+                if(method === 'exit' || method.length === 0) {
+                    saveFile();
+                    rl.close()
+                } else {
+                    console.log(chalk.cyan.bold('Creating Factory named: %s for url: %s'), method.capitalize(), baseURL + path);
+                    console.log(chalk.cyan.bold("with methods: \ncreate()\nupdate\nfindAll()\nfindById()\ndelete()\n"));
+                    factories.push({method: method.capitalize(), url: path});
+                    nameMethods()
+                }
+            })
+        }
+    })
+};
+
+var saveFile = function() {
+    if (factories.length > 0){
+        var template;
+        if (program.auth){
+            template = ejs.render(authTemplate, {
+                moduleName: moduleName,
+                models: factories,
+                urlBase: baseURL
+            });
+        } else {
+            template = ejs.render(servicesTemplate, {
+                moduleName: moduleName,
+                models: factories,
+                urlBase: baseURL
+            });
+        }
+
+        fs.writeFile(filename, template, function (err) {
+            if (err) throw err;
+            console.log(chalk.cyan.bold('Saving resources file to: ' + filename));
+            process.exit(0);
+        });
+    } else {
+        console.log(chalk.red.bold('Nothing to save. Aborting!'));
+        process.exit(1);
+
+    }
+
+};
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
